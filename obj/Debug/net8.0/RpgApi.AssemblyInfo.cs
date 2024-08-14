c61178f644503093708aa4a8fe163d4621e78da7
gfferreiira <arzhun7381@gmail.com>
2024-08-13 22:14:48 -0300

    Adicionando a API na Nuvem da azure e configurando no APPsetings

--- obj/Debug/net8.0/RpgApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/RpgApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RpgApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4ed70a039a7d2acbf9bee324ce43610afcccf22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d73ec74a25205537d980e2f95deec91590ee00bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("RpgApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RpgApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
